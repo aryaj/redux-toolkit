4d027736bd4f7e39400442443b1cf5869b5d7778
Arya <ajaarya@apac.corpdir.net>
2022-09-14 11:52:06 +0530

    type of createRoot corrected

--- react-rtk-ts-demo/src/main.tsx
+++ react-rtk-ts-demo/src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from './app/store';
 
-ReactDOM.createRoot(document.getElementById("root") as d).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
